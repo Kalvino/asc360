0c23fc2744585118a5c55ab7813bb4950e3365dc
Chris McKenna <topher@host01.(none)>
2013-06-11 10:59:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asc360::Application.config.secret_token = '45e4cdf23b3aa5dfb68de24e6d30060c97c37efe93b95be99d6ac712dd378e4647d6bf0b805ec16eb160f619c97cc2468dc94fd1d79833ea1dc7fd26d4fe9504'
